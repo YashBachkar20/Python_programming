ddb977fcf206f3cb2c1b6b3382bb6ec9c25ee970
YashBachkar20 <yashbachkar20@gmail.com>
2023-10-18 21:27:32 +0530

    selcious to faherinhit

---
+++ selcious_to_faherinhit.py
@@ -0,0 +1,7 @@
+#approch 1:
+
+#faheriheit = (celsius * 1.8) + 32  
+
+celsius = 37.5
+faheriheit = (celsius * 1.8) + 32
+print("%0.1f degree faheinhit is  equal to %0.1f degree celsius :" %(celsius,faheriheit))
